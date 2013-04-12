5dd664c41067164aa5fcfb3aebd5a676223cd6aa
U-ck-laptop\ck <ck@ck-laptop.(none)>
2013-04-12 18:11:41 +0900

    Loading message added for Tineye and Karmadecay Version 0.2.5

--- scripts/contentscripts.js
+++ scripts/contentscripts.js
@@ -1,7 +1,8 @@
 var fillAndSubmit = function(formSelector, inputSelector) {
-  var $form = $(formSelector);
-  $form.find(inputSelector).val(location.hash.slice(1));
+  var $form = $(formSelector), url = location.hash.slice(1);
+  $form.find(inputSelector).val(url);
   $form.submit();
+  $('body').html('<div style="width:500px;margin:200px auto;">Searching "' + url + '"...</div>');
 };
 
 switch(location.host) {
