e6aacbc5744eba930ece76e09a9e9cd4f561d428
U-ck-laptop\ck <ck@ck-laptop.(none)>
2013-04-30 14:30:49 +0900

    Code cleaned Version 0.4.1

--- scripts/background.js
+++ scripts/background.js
@@ -1,40 +1,20 @@
 var getClickHandler = function(website) {
   return function(info) {
-    var src = info.srcUrl, url = '';
-    switch(website) {
-      case 'google': url = 'http://images.google.com/searchbyimage?image_url=' + src; break;
-      case 'tineye': url = 'http://www.tineye.com/search?url=' + src; break;
-      case 'karmadecay': url = 'http://karmadecay.com/search?q=' + src; break;
-      case 'imgops': url = 'http://imgops.com/' + src; break;
-    }
-    chrome.tabs.create({url: url});
+    chrome.tabs.create({url: website.replace('%s', info.srcUrl)});
   };
 };
 
-chrome.contextMenus.create({
-  'title' : 'Google Images',
-  'type' : 'normal',
-  'contexts' : ['image'],
-  'onclick' : getClickHandler('google')
-});
+var engines = {
+  'Google Images': 'http://images.google.com/searchbyimage?image_url=%s',
+  'Tineye': 'http://www.tineye.com/search?url=%s',
+  'KarmaDecay': 'http://karmadecay.com/search?q=%s',
+  'Open With ImgOps': 'http://imgops.com/%s'
+};
 
-chrome.contextMenus.create({
-  'title' : 'Tineye',
-  'type' : 'normal',
-  'contexts' : ['image'],
-  'onclick' : getClickHandler('tineye')
-});
-
-chrome.contextMenus.create({
-  'title' : 'KarmaDecay',
-  'type' : 'normal',
-  'contexts' : ['image'],
-  'onclick' : getClickHandler('karmadecay')
-});
-
-chrome.contextMenus.create({
-  'title' : 'Open With ImgOps',
-  'type' : 'normal',
-  'contexts' : ['image'],
-  'onclick' : getClickHandler('imgops')
-});
+for(title in engines)
+  chrome.contextMenus.create({
+    'title': title,
+    'type': 'normal',
+    'contexts': ['image'],
+    'onclick': getClickHandler(engines[title])
+  });
